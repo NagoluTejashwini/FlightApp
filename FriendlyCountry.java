3460501d7410361a85d51c172b90df45d230807c
Nagolu Tejashwini <52523493+NagoluTejashwini@users.noreply.github.com>
2021-04-12 17:55:10 +0530

    Add files via upload

---
+++ FriendlyCountry.java
@@ -0,0 +1,13 @@
+package day4.task4;
+
+public class FriendlyCountry {
+	public String countries[] =
+		{
+				"UAE",
+				"Nepal",
+				"Bhutan",
+				"Mauritius",
+				"Afganistan",
+				"Mangolia",
+		};
+}
